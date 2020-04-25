13f08965467693ce78e7bd26ccb35b5071dc245e
Shunsuke <shun.sskd05@icloud.com>
2020-04-25 13:07:28 +0000

    [Add] ログインフォーム

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user)
+      redirect_to controller: :users, action: :show
     else
       render "new"
     end
